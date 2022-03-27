ddac0e185640a4e11ecd6cfc631074bf1bd1853d
Cysmi <cnb10100@gmail.com>
2022-03-26 22:19:12 -0400

    setup controllers structure

---
+++ controllers/thoughts-controller.js
@@ -0,0 +1,7 @@
+// Require Thoughts and Users Models
+const {Thoughts, Users} = require('../models');
+
+const thoughtsController = {}
+
+// Export Thoughts-Controller
+module.exports = thoughtsController;
---
+++ controllers/users-controller.js
@@ -0,0 +1,7 @@
+// Require Users Model
+const {Users} = require('../models');
+
+const usersController = {}
+
+// Export Users-Controller
+module.exports = usersController; 
