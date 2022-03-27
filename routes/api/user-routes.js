dc445c148a564156f9f09df8270793391b385564
Cysmi <cnb10100@gmail.com>
2022-03-27 17:37:09 -0400

    finished routes and updated server

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,25 @@
 // Require express router
-const router = require('express').Router();
+const router = require("express").Router();
+
+// Require Users-Controller
+const {
+  getAllUsers,
+  getUsersById,
+  createUsers,
+  updateUsers,
+  deleteUsers,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/users-controller");
+
+// /api/users <GET, POST>
+router.route('/').get(getAllUsers).post(createUsers);
+
+//  /api/users/:id <GET, PUT, DELETE>
+router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+
+// /api/users/:userId/friends/:friendId <POST, DELETE>
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
 
 // Export router
-module.exports = router; 
+module.exports = router;
